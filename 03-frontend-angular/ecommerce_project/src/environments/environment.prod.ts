56f3a933e35d51a02f1b3b76e132ad0bfeb10524
hardik830 <hardkhandelwal9636@gmail.com>
2024-08-22 11:10:12 +0530

    changes done in  environment prod file

--- 03-frontend-angular/ecommerce_project/src/environments/environment.prod.ts
+++ 03-frontend-angular/ecommerce_project/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+   luv2shopApiUrl: "https://localhost:8080/api",
+     stripePublishableKey: "pk_test_51PozZM07HAUjgHm6yaH6QAVdqwyMAQ8ITpH5Az703RRTd3qqy0Y960Pn2OvIz2siEcoRmLyqO2hhbiuQVYj6ZTRM00LsocbsnU"
 };
--- 03-frontend-angular/ecommerce_project/src/environments/environment.ts
+++ 03-frontend-angular/ecommerce_project/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-    luv2shopApiUrl: "https://localhost:8443/api",
+    luv2shopApiUrl: "https://localhost:8080/api",
      stripePublishableKey: "pk_test_51PozZM07HAUjgHm6yaH6QAVdqwyMAQ8ITpH5Az703RRTd3qqy0Y960Pn2OvIz2siEcoRmLyqO2hhbiuQVYj6ZTRM00LsocbsnU"
 };
 
